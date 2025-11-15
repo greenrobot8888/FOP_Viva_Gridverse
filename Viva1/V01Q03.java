3b1bcff0cebee6ddd02af3ad0e2da7b0d1c4d63e
Muhammad Dzaky Aryasatya <24086876@siswa.um.edu.my>
2025-11-15 11:29:44 +0800

    Improve input prompts and validation comments

--- Viva1/V01Q03.java
+++ Viva1/V01Q03.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 public class V01Q03 {
     public static void main(String[] args) {
-     Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         
-        System.out.print("Please enter the number of inquaries: ");
+        System.out.print("Please enter the number of inquaries (1-50): ");
         int T = input.nextInt();
-        
+        //The number of inquiries, T, will be between 1 and 50. 
+        if(T>=1&&T<=10){ 
         for(int d=1; d<=T;d++){
              
-        System.out.print("Please enter the Height: ");
+        System.out.print("Please enter the Height (1-9): ");
         int H = input.nextInt();
         System.out.print("Please enter the Style (A/P): ");
         String S = input.next();
-        
+        //the number of H will be 1-9 or else its invalid
+        if(H>=1&&H<=9){
         if(S.equals("A")){
             for(int i=1;i<=H;i++){
                 for(int u=1;u<=i;u++){
@@ -33,7 +35,8 @@ public class V01Q03 {
                     System.out.print(r);
                 }System.out.println();
             }
-        }
+        }}
     }
-    }  
+    }
+    }    
 }
