645e0cab6d8dfaf1ea9e568b2f59f1bed2da0ccf
Muhammad Dzaky Aryasatya <24086876@siswa.um.edu.my>
2025-11-15 11:15:39 +0800

    Add V01Q03.java

---
+++ Viva1/V01Q03.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class V01Q03 {
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+        
+        System.out.print("Please enter the number of inquaries: ");
+        int T = input.nextInt();
+        
+        for(int d=1; d<=T;d++){
+             
+        System.out.print("Please enter the Height: ");
+        int H = input.nextInt();
+        System.out.print("Please enter the Style (A/P): ");
+        String S = input.next();
+        
+        if(S.equals("A")){
+            for(int i=1;i<=H;i++){
+                for(int u=1;u<=i;u++){
+                    System.out.print(i);
+                }
+                System.out.println();
+            }       
+    }
+        if(S.equals("P")){
+            for(int i=1;i<=H;i++){
+                for(int s=H;s>=i;s--){
+                    System.out.print(" ");
+                }
+                for(int u=1;u<=i;u++){
+                    System.out.print(u);
+                }
+                for(int r=i-1;r>=1;r--){
+                    System.out.print(r);
+                }System.out.println();
+            }
+        }
+    }
+    }  
+}
