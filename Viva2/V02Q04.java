7c3d53ac95a863152d057bb4682d215214feeaf0
Nazeef Nuwaisir Khan <nazeef05khan@gmail.com>
2025-12-14 14:56:42 +0800

    Clean up V02Q04.java by removing comments
    Removed unnecessary comments and package declaration.

--- Viva2/V02Q04.java
+++ Viva2/V02Q04.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package V02;
-
-/**
- *
- * @author Dzaky Arya
- */
 import java.util.Scanner;
+
 public class V02Q4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
